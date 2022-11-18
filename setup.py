1a647a11fe253daf66b71a9b2d17e4111a3a0cf5
arfy slowy <slowy.arfy@gmail.com>
2022-11-18 19:35:54 +0700

    fix: rolling back ``setup``

--- setup.py
+++ setup.py
@@ -1,37 +1,12 @@
-from os import path as op
 from setuptools import find_packages, setup
-import io
-
-with open("README.rst") as readme_file:
-    readme = readme_file.read()
-
-here = op.abspath(op.dirname(__file__))
-
-with io.open(op.join(here, "requirements.txt"), encoding="utf-8") as f:
-    all_reqs = f.read().split("\n")
-
-install_requires = [x.strip() for x in all_reqs if "git+" not in x]
-dependency_links = [x.strip().replace("git+", "") for x in all_reqs if "+git" not in x]
-
-setup_requires = []
 
 setup(
-    name="derune-luna",
+    name="Luna",
     version="1.0.1",
-    python_requires=">=3.7",
-    classifiers=[
-        "Intended Audience :: Developers",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-    ],
-    author="arfy slowy",
+    description="text to image with luna",
+    author="Arfy slowy",
     author_email="slowy.arfy@gmail.com",
-    platform=["any"],
-    long_description=readme,
-    keywords="luna stable diffusion",
+    platforms=["any"],
     url="https://github.com/De-Rune/luna",
     packages=find_packages(),
 )
